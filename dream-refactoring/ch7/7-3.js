eecfa70e0f585a3c86b37e61313a598f40bfb7af
hoki <hoki@windmillsoft.kr>
2024-10-31 13:11:32 +0900

    기본형을 객체로 바꾸기

--- dream-refactoring/ch7/7-3.js
+++ dream-refactoring/ch7/7-3.js
@@ -1,15 +1,46 @@
 export class Order {
-  constructor(data) {
-    this.priority = data.priority;
+  constructor(priority) {
+    this.priority = priority;
+  }
+
+  isHighPriority() {
+    return this.priority.equals(new Priority('high')) || this.priority.equals(new Priority('rush'));
+  }
+}
+
+class Priority {
+  #value;
+  constructor(value) {
+    if(Priority.legalValue().includes(value)) {
+      this.#value = value;
+    } else {
+      throw new Error(`${value} is invalid for Priority`);
+    }
+  }
+
+  get index() {
+    return Priority.legalValue().indexOf(this.#value);
+  }
+
+  equals(other) {
+    return this.index === other.index;
+  }
+
+  higherThan(other) {
+    return this.index > other.index;
+  }
+
+  static legalValue() {
+    return ['low', 'normal', 'high', 'rush'];
   }
 }
 
 const orders = [
-  new Order({ priority: 'normal' }),
-  new Order({ priority: 'high' }),
-  new Order({ priority: 'rush' }),
+  new Order(new Priority('normal')),
+  new Order(new Priority('high')),
+  new Order(new Priority('rush')),
 ];
 
-const highPriorityCount = orders.filter(
-  (o) => 'high' === o.priority || 'rush' === o.priority
-).length;
+const highPriorityCount = orders.filter( (o) => o.isHighPriority() ).length;
+
+console.log(highPriorityCount);
