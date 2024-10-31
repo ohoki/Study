7da863c21e06a59655256505e1fe524438f90b31
hoki <hoki@windmillsoft.kr>
2024-10-31 10:37:25 +0900

    레코드 캡슐화하기

--- dream-refactoring/ch7/7-1.js
+++ dream-refactoring/ch7/7-1.js
@@ -1,5 +1,22 @@
-const organization = { name: 'Acme Gooseberries', country: 'GB' };
+class Organization {
+    #name;
+    #country;
+    constructor(name, country) {
+        this.#name = name;
+        this.#country = country;
+    }
 
-organization.name = 'Dream Coding';
+    get name() {
+        return this.#name;
+    }
+
+    get country() {
+        return this.#country;
+    }
+}
+
+const organization = new Organization('Acme Gooseberries', 'GB');
+
+// organization.name = 'Dream Coding';
 console.log(organization.name);
 console.log(organization.country);
