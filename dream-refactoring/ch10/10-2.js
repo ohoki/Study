c5132e9a0839f23b59db71fb300faf65d8989c48
hoki <hoki@windmillsoft.kr>
2024-11-13 08:09:54 +0900

    조건식 통합하기

--- dream-refactoring/ch10/10-2.js
+++ dream-refactoring/ch10/10-2.js
@@ -1,6 +1,15 @@
 function disabilityAmount(employee) {
-  if (employee.seniority < 2) return 0;
-  if (employee.monthsDisabled > 12) return 0;
-  if (employee.isPartTime) return 0;
-  return 1;
+  if(isNotEligibleForDisability()) {
+    return 0;
+  } else {
+    return 1;
+  }
+}
+
+function isNotEligibleForDisability(employee) {
+  return (
+    employee.seniority < 2 ||
+    employee.monthsDisabled > 12 ||
+    employee.isPartTime
+  );
 }
