5ebdd89c2aab0fb2fe06b8070fa216e30bd8020a
hoki <hoki@windmillsoft.kr>
2024-11-13 10:11:46 +0900

    어서션 추가하기

--- dream-refactoring/ch10/10-6.js
+++ dream-refactoring/ch10/10-6.js
@@ -1,8 +1,11 @@
+import { strict as assert } from 'node:assert';
+
 class Customer {
   constructor() {
     this.discountRate = 0;
   }
   applyDiscount(number) {
+    assert(number >= 0);
     return this.discountRate ? number - this.discountRate * number : number;
   }
 }
