278a715d027d885504a8c4bf67055e4dc2308927
hoki <hoki@windmillsoft.kr>
2024-11-20 17:39:51 +0900

    매개변수를 질의 함수로 바꾸기

--- dream-refactoring/ch11/11-5.js
+++ dream-refactoring/ch11/11-5.js
@@ -4,20 +4,24 @@ export class Order {
     this.itemPrice = itemPrice;
   }
 
+  get basePrice() {
+    return this.quantity * this.itemPrice;    
+  }
+ 
   get finalPrice() {
-    const basePrice = this.quantity * this.itemPrice;
-    let discountLevel;
-    if (this.quantity > 100) discountLevel = 2;
-    else discountLevel = 1;
-    return this.discountedPrice(basePrice, discountLevel);
+    return this.discountedPrice();
   }
 
-  discountedPrice(basePrice, discountLevel) {
-    switch (discountLevel) {
+  get discountLevel() {
+    return this.quantity > 100 ? 2 : 1;
+  }
+
+  discountedPrice() {
+    switch (this.discountLevel) {
       case 1:
-        return basePrice * 0.95;
+        return this.basePrice * 0.95;
       case 2:
-        return basePrice * 0.9;
+        return this.basePrice * 0.9;
     }
   }
 }
