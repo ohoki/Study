08d2fef96a26f6b8db57f55e3e59b539cecb229a
hoki <hoki@windmillsoft.kr>
2024-11-26 09:16:32 +0900

    메서드 내리기

--- dream-refactoring/ch12/12-4.js
+++ dream-refactoring/ch12/12-4.js
@@ -1,6 +1,7 @@
-class Employee {
-  get quota() {}
-}
+class Employee {}
 
 class Engineer extends Employee {}
-class Salesperson extends Employee {}
+class Salesperson extends Employee {
+  //quota()가 Salesperson에서만 사용되는 메서드인 경우 
+  get quota() {}
+}
